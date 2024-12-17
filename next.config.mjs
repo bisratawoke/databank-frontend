0ddba5ec9adbc6689350fc86a9f8e9bdd9124b09
bisratawoke <bisratawoke52@gmail.com>
2024-12-17 13:04:21 +0300

    Added deployment static ip to the remote patterns config

--- next.config.mjs
+++ next.config.mjs
@@ -13,6 +13,11 @@ const nextConfig = {
         hostname: "127.0.0.1",
         port: "9000",
       },
+      {
+        protocol: "http",
+        hostname: "196.188.175.230",
+        port: "9000",
+      },
     ],
   },
 };
